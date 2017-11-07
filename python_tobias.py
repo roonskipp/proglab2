60ae0c294495bee42f757efca550b7fb3f671e96
Tobias <trampcha@gmail.com>
2017-11-07 13:00:30 +0100

    Tobias fil, sensoboppdatering
    videre arbeid med sensob classen i min workfile

--- python_tobias.py
+++ python_tobias.py
@@ -3,8 +3,39 @@ class BBCON():
     pass
 
 class Sensob():
-    # TODO
-    pass
+    self.object = None
+
+    def __init__(self, type):
+        self.object = type
+
+    def update(self):
+        self.object.update()
+
+    def getValue(self):
+        self.object.get_value()
+
+    def reset(self):
+        self.object.reset()
+
+    def returnValue(self):
+        self.update()
+        value = self.getValue()
+
+        if isinstance(value, list):
+            # sensob-objektet jobber med en IR-leser
+            # TODO
+            pass
+
+        elif isinstance(value, float):
+            # sensob-objektet jobber med ultrasonic-leser
+            # TODO
+            pass
+
+        else:
+            # sensob-objektet jobber med kameraet
+            # TODO
+            pass
+
 
 class Motob():
     # TODO
