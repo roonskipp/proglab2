4c8159c7e32aef2048943c11cf8ff08d359423b5
Tobias <trampcha@gmail.com>
2017-11-07 11:36:33 +0100

    # TODO
    La til # TODO

--- proj6.py
+++ proj6.py
@@ -2,21 +2,26 @@
 
 
 class BBCON:
+    # TODO
     pass
 
 
 class Sensob:
+    # TODO
     pass
 
 
 class Motob:
+    # TODO
     pass
 
 
 class Behavior:
+    # TODO
     pass
 
 
 class Arbitrator:
+    # TODO
     pass
 
