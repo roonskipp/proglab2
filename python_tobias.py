e1c4a614a751349990fd2a0fa5436d71d531bba4
Tobias <trampcha@gmail.com>
2017-11-07 11:35:41 +0100

    Første push av denne filen, hvis dere vil se (test)
    Descirption, please ignore.

--- python_tobias.py
+++ python_tobias.py
@@ -1,1 +1,19 @@
+class BBCON():
+    # TODO
+    pass
 
+class Sensob():
+    # TODO
+    pass
+
+class Motob():
+    # TODO
+    pass
+
+class Behavior():
+    # TODO
+    pass
+
+class Arbitrator():
+    # TODO
+    pass
