60a885aca7671b5dbc97505f22e281b06c70c79b
magnusrand <magnus.rand@gmail.com>
2017-11-07 12:13:23 +0100

    instanser og metoder for BBCON

--- proj6.py
+++ proj6.py
@@ -2,8 +2,30 @@
 
 
 class BBCON:
-    # TODO
-    pass
+
+    def __init__(self, behav=None, sensob=None, motobs=None, arbitrator=None):
+        self.behaviors = [] if behav is None else behav
+        self.act_behaviors = []
+        self.sensor_objs = [] if sensob is None else sensob
+        self.motor_objs = [] if motobs is None else motobs
+        self.arbitrator = Arbitrator() if arbitrator is None else arbitrator
+
+    def add_behavior(self, behavior):
+        self.behaviors.append(behavior)
+
+    def add_sensob(self, sensob):
+        self.sensor_objs.append(sensob)
+
+    def activate_behavior(self, behavior):
+        self.act_behaviors.append(behavior)
+
+    def deactivate_behavior(self, behavior):
+        if behavoir in self.act_behaviors:
+            self.act_behaviors.remove(behavior)
+
+    def run_one_timestep(self):
+        #TODO
+        pass
 
 
 class Sensob:
