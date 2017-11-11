05aa142c208139a62c628b73a4495f861623736d
magnusrand <magnus.rand@gmail.com>
2017-11-11 15:02:31 +0100

    oppdaterer fra proj6

--- python_magnus.py
+++ python_magnus.py
@@ -1,6 +1,9 @@
 # Oving 6
 import random
 
+
+# update
+
 class BBCON:
     def __init__(self, behav=None, sensob=None, motobs=None, arbitrator=None):
         self.behaviors = [] if behav is None else behav
@@ -91,51 +94,134 @@ class Motob:
         self.value = recommendation
         self.operationalize()
 
-    def operationalize(self, operation=None):  # Skal utføre motoroperasjonen den har fått, enten via operation eller self.value fra update. Endrer alle motorer i motorlisten til objektet
+    def operationalize(self,
+                       operation=None):  # Skal utføre motoroperasjonen den har fått, enten via operation eller self.value fra update. Endrer alle motorer i motorlisten til objektet
         use_operation = operation
         if operation is None:
             use_operation = self.value
         for motor in self.motors:
-            motor.set_value(use_operation) # TODO Kan hende vi må endre eller legge til en hjelpeklasse her for å få det til å funke
+            motor.set_value(
+                use_operation)  # TODO Kan hende vi må endre eller legge til en hjelpeklasse her for å få det til å funke
 
 
 class Behavior:
-    #   Bare en start, mye å gjøres.
     #   Classvariables
-    def __init__(self, bbcon, sensobs, motor_recommendations, active_flag, halt_request, priority, match_degree,
-                 weight):
-        bbcon = bbcon  # BBCON objektet som denne behavioren hører til
-        sensobs = sensobs  # Liste over de sensob-objektene som behavior-objektet bruker
-        motor_recommendations = motor_recommendations  # Liste over recommendations en per motob, sendes til arbitratoren
-        active_flag = active_flag  # Boolean som forteller om denne behavioren er aktiv eller ikke
-        halt_request = halt_request  # UVISST?? Se dokumentasjonen på behavior i oppgaven
-        priority = priority  # Statisk, forteller viktigheten til denne behavioren
-        match_degree = match_degree  # Reelttall, mellom 0 og 1, sier noe om hvor naturlig det er å gjøre denne handlingen
-        weight = weight  # Produktet av priority og match_degree, brukes for av arbitrator for å bestemme hvilken behavior som skal utføres.
+    def __init__(self, bbcon, sensobs, motor_recommendations, active_flag, halt_request, priority, match_degree, weight,
+                 behavior_number):
+        self.bbcon = bbcon  # BBCON objektet som denne behavioren hører til
+        self.sensobs = sensobs  # Liste over de sensob-objektene som behavior-objektet bruker
+        self.motor_recommendations = motor_recommendations  # Liste over recommendations en per motob, sendes til arbitratoren
+        self.active_flag = active_flag  # Boolean som forteller om denne behavioren er aktiv eller ikke
+        self.halt_request = halt_request  # UVISST?? Se dokumentasjonen på behavior i oppgaven
+        self.priority = priority  # Statisk, forteller viktigheten til denne behavioren
+        self.match_degree = match_degree  # Reelttall, mellom 0 og 1, sier noe om hvor naturlig det er å gjøre denne handlingen
+        self.weight = weight  # Produktet av priority og match_degree, brukes for av arbitrator for å bestemme hvilken behavior som skal utføres.
+        self.behavior_number = behavior_number  # For å vite hvilken behavior de ulike er.
+
+    def consider_activation(self):
+        # TODO
+
+        values = []
+        for sensob in self.sensobs:
+            values.append(sensob.getValue())
+
+        if self.behavior_number == 1:
+            # TODO
+            pass
+
+        elif self.behavior_number == 2:
+            # TODO
+            pass
+
+        elif self.behavior_number == 3:
+            # TODO
+            pass
+
+    def consider_deavtivation(self):
+        # TODO
+
+        values = []
+        for sensob in self.sensobs:
+            values.append(sensob.getValue())
+
+        if self.behavior_number == 1:
+            # TODO
+            pass
+
+        elif self.behavior_number == 2:
+            # TODO
+            pass
+
+        elif self.behavior_number == 3:
+            # TODO
+            pass
+
+    def update(self):
+        # TODO
+        # Sjekke om behavioren er aktiv eller ikke
+
+        if self.active_flag == True:
+            if self.consider_deactivation() == False:
+                # Behavior er aktiv og skal forbli aktiv.
+                self.sense_and_act()
+
+            elif self.consider_deactivation() == True:
+                # Behavior er aktiv, men skal deaktiveres
+                self.active_flag = False
+
+        elif self.active_flag == False:
+            if self.consider_activation() == True:
+                self.active_flag = True
+                self.sense_and_act()
+
+    def sense_and_act(self):
+        # TODO
+        # Må for hver behavior se på values ( sensor data ) og lage motob recommendations, og legge disse i sin egen liste
+        # over motob recommendations. Må også oppdatere match_degree basert på dataen, og oppdatere sin egen match degree.
+        values = []
+        for sensob in self.sensobs:
+            values.append(sensob.getValue())
+
+        if self.behavior_number == 1:
+            # TODO
+            pass
+
+        elif self.behavior_number == 2:
+            # TODO
+            pass
+
+        elif self.behavior_number == 3:
+            # TODO
+            pass
+
+        pass
 
 
 class Arbitrator:
-
     def __init__(self, bbcon):
         self.bbcon = bbcon
 
-    def choose_action(self): # Skal ta inn alle aktive behavoirs og velge én av de
+    def choose_action(self):  # Skal ta inn alle aktive behavoirs og velge én av de
         cur_val = 0
         intervall = []
         for act_behv in self.bbcon.act_behaviors:
-            intervall.append([cur_val, cur_val + act_behv.weight])  # Lager intervall med størresle bassert på vekten deres. Dvs. hvis du har to behaviors med vekt 0.8 og 0.5 vil intervallet bli [[0, 0.8],[0.8, 1.3]]
+            intervall.append([cur_val,
+                              cur_val + act_behv.weight])  # Lager intervall med størresle bassert på vekten deres. Dvs. hvis du har to behaviors med vekt 0.8 og 0.5 vil intervallet bli [[0, 0.8],[0.8, 1.3]]
             cur_val = cur_val + act_behv
-        win_weight = random.randrange(intervall[-1][-1]) # Velger tilfeldig ut en verdi innenfor intervallet. Større intervall vil da ha større sanns. for å vinne
+        win_weight = random.randrange(intervall[-1][
+                                          -1])  # Velger tilfeldig ut en verdi innenfor intervallet. Større intervall vil da ha større sanns. for å vinne
         for i in range(len(intervall)):
             if win_weight >= intervall[i][0]:
-                if win_weight <= intervall[i][1]: # Sjekker hvilket intervall tallet havnet innenfor og returnerer indexen
+                if win_weight <= intervall[i][
+                    1]:  # Sjekker hvilket intervall tallet havnet innenfor og returnerer indexen
                     win_index = i
                     break
         win_behv = self.bbcon.act_behaviors[i]
         return_val = []
-        for mr in win_behv.motor_recommendations: # Lager en liste med alle motorrecommendations for winner behavior pluss en boolean for halt_request på slutten
+        for mr in win_behv.motor_recommendations:  # Lager en liste med alle motorrecommendations for winner behavior pluss en boolean for halt_request på slutten
             return_val.append(mr)
         return_val.append(win_behv.halt_request)
 
         return return_val
+
 
